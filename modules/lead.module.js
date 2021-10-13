ac3b5ec2894b0e45e0fc240aee8498e96f85acce
Ivan Andreiev <ivan.andreiev@ffflabel.com>
2021-10-13 21:09:27 +0300

    mailer

---
+++ modules/lead.module.js
@@ -0,0 +1,40 @@
+let selected = ''
+const BASE = "http://localhost:3031"
+
+
+async function getFromFields() {
+    let name = document.getElementById('form-name').value,
+        phone = document.getElementById('form-phone').value,
+        email = document.getElementById('form-email').value,
+        comment = document.getElementById('form-comment').value;
+
+    let options = {
+        method: 'post',
+        url: BASE + '/mail/lead',
+        header: {
+            'Content-type': "application/x-www-form-urlencoded"
+        },
+        body: {
+            name: name,
+            phone: phone,
+            email: email,
+            connect: selected,
+            comment: comment
+        }
+    };
+
+
+    axios.post(options.url, options.body)
+        .then((res) => {
+            console.log(options)
+            console.log(res)
+        })
+        .catch((err) => {
+            console.log(err)
+        })
+
+}
+
+function radioSelect(option) {
+    selected = option
+}
